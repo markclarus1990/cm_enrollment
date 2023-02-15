6c9143b9f137b77eeaa59a6705d9b3c7e6d238b9
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2023-02-15 23:01:40 +0800

    adding hello comment in store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import goalReducer from '../features/goals/goalSlice'
-
+//hello
 export const store = configureStore({
   reducer: {
     auth: authReducer,
